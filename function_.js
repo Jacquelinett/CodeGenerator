084fb0e40ce7c6b9090d16884317b1549e447662
backwards-dolphin <cfe5057@psu.edu>
2017-04-08 16:05:07 -0400

    preliminary data

---
+++ function_.js
@@ -0,0 +1,20 @@
+class function_{
+    constructor(x,y){
+        this.name = x;
+        this.comment = y;
+    }
+    getName(){
+        return this.name;
+    }
+    getComment(){
+        return this.comment;
+    }
+    setName(name){
+        this.name = name;
+    }
+    setComment(comment){
+        this.comment = comment;
+    }
+    
+    //var obj = new function_("test","test");
+}
